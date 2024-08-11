3514b61e179b9ee2f87a75a520339845a141a1bb
Tatuu5 <tatu_rusanen@hotmail.fi>
2024-08-12 02:58:26 +0300

    user creation done

--- osa4/bloglist/app.js
+++ osa4/bloglist/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogRouters')
+const usersRouter = require('./controllers/usersRouter')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
@@ -25,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 module.exports = app
